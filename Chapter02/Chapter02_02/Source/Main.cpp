644982a3b2237cdfeff8011a497739ce34cf557e
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-05-31 12:06:25 +0900

    전체적인 레이트레이싱 결과 이미지를 생성하는 프로그램 구조 추가

--- Chapter02/Chapter02_02/Source/Main.cpp
+++ Chapter02/Chapter02_02/Source/Main.cpp
@@ -1,27 +1,12 @@
-#include <iostream>
+#include "Raytracer.h"
 
 int main(void)
 {
-	int image_width = 256;
-	int image_height = 256;
+	Raytracer raytracer;
 
-	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
-	for (int j = 0; j < image_height; ++j)
-	{
-		for (int i = 0; i < image_width; ++i)
-		{
-			double r = static_cast<double>(i) / static_cast<double>(image_width - 1);
-			double g = static_cast<double>(j) / static_cast<double>(image_height - 1);
-			double b = 0.0;
-
-			int ir = static_cast<int>(255.999 * r);
-			int ig = static_cast<int>(255.999 * g);
-			int ib = static_cast<int>(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
-		}
-	}
+	raytracer.Initialize();
+	raytracer.Run();
+	raytracer.Export();
 
 	return 0;
 }
---
+++ Chapter02/Chapter02_02/Source/Raytracer.cpp
@@ -0,0 +1,13 @@
+#include "Raytracer.h"
+
+void Raytracer::Initialize()
+{
+}
+
+void Raytracer::Run()
+{
+}
+
+void Raytracer::Export()
+{
+}
