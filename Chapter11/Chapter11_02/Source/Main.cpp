540e9176e3619da78add190e54172c882a332b67
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2025-06-08 13:14:47 +0900

    Chapter11 실습 코드 추가

--- Chapter11/Chapter11_02/Source/Main.cpp
+++ Chapter11/Chapter11_02/Source/Main.cpp
@@ -11,7 +11,7 @@ int main(void)
 {
 	std::shared_ptr<lambertian> material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
 	std::shared_ptr<lambertian> material_center = std::make_shared<lambertian>(color(0.1, 0.2, 0.5));
-	std::shared_ptr<dielectric> material_left = std::make_shared<dielectric>(1.50);
+	std::shared_ptr<dielectric> material_left = std::make_shared<dielectric>(1.00 / 1.33);
 	std::shared_ptr<metal> material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
 	hittable_list world;
--- Chapter11/Chapter11_03/Source/material.h
+++ Chapter11/Chapter11_03/Source/material.h
@@ -69,10 +69,31 @@ public:
 		double ri = record.front_face ? (1.0 / _refraction_index) : _refraction_index;
 
 		vec3 unit_direction = unit_vector(r_in.direction());
-		vec3 refracted = refract(unit_direction, record.normal, ri);
 
-		scattered = ray(record.p, refracted);
+		double cos_theta = std::fmin(dot(-unit_direction, record.normal), 1.0);
+		double sin_theta = std::sqrt(1.0 - cos_theta * cos_theta);
+
+		bool cannot_refract = ri * sin_theta > 1.0;
+		vec3 direction;
+		if (cannot_refract || reflectance(cos_theta, ri) > random_double())
+		{
+			direction = reflect(unit_direction, record.normal);
+		}
+		else
+		{
+			direction = refract(unit_direction, record.normal, ri);
+		}
+		
+		scattered = ray(record.p, direction);
 		return true;
+	}
+
+	// https://en.wikipedia.org/wiki/Schlick%27s_approximation
+	static double reflectance(double cosine, double refraction_index)
+	{
+		double r0 = (1 - refraction_index) / (1 + refraction_index);
+		r0 = r0 * r0;
+		return r0 + (1.0 - r0) * std::pow((1.0 - cosine), 5);
 	}
 
 private:
